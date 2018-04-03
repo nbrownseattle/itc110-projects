938f31e4faf3d0dc8ed52edc35345c6e643e9a94
nbrownseattle <nbrown28@seattlecentral.edu>
2018-04-03 12:32:03 -0700

    Adds files for assignment 8

---
+++ Assignment8_nbrown/Assignment8_nbrown/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment8_nbrown
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSbmit_Click(object sender, RoutedEventArgs e)
+        {
+            tipCalculator m = new tipCalculator();
+
+            int tipAmount;
+            if(radioButtonten.IsChecked==true)
+            {
+                tipAmount = 10;
+            }
+            else if (radioButtonfifteen.IsChecked==true)
+            {
+                tipAmount = 15;
+            }
+            else if (radioButtontwenty.IsChecked==true)
+            {
+                tipAmount = 20;
+            }
+            else
+            {
+                tipAmount = int.Parse(textBoxother.Text);
+            }
+            m.Tip = tipAmount;
+            m.Meal = double.Parse(textBoxmealamount.Text);
+            textBoxtax.Text = m.GetTax().ToString();
+            textBoxtip.Text = m.Tip.ToString();
+            textBoxtotal.Text = m.CalculateTotal().ToString();
+        }
+
+       
+    }
+}
---
+++ Assignment8_nbrown/Assignment8_nbrown/tipCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8_nbrown
+{
+    public class tipCalculator
+    {
+        private double mealAmount;
+        private double tipAmount;
+        public tipCalculator()
+        {
+            Meal = 0;
+            Tip = 0;
+        }
+
+        public tipCalculator(double mealAmount, double tipAmount)
+        {
+            Meal = mealAmount;
+            Tip = tipAmount;
+        }
+
+        public double Meal
+        {
+            set { mealAmount = value; }
+            get { return mealAmount; }
+        }
+        public double Tip
+        {
+            get { return tipAmount; }
+            set { tipAmount = value; }
+        }
+        public double GetTax()
+        {
+            return Meal * .09;
+        }
+        public double CalculateTip()
+        {
+            return Meal * (Tip/100);
+        }
+        public double CalculateTotal()
+        {
+            return CalculateTip() + GetTax() + Meal;
+        }
+    }
+}
