d9090833c151cb7d6f97fe3a57245ce3db2ec35a
nbrownseattle <nbrown28@seattlecentral.edu>
2018-04-03 11:58:42 -0700

    Adds files for assignment 4-2

---
+++ Assignment4_2_nbrown/Assignment4_2_nbrown/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsPractice_nbrown
+{
+    class Program
+    {
+        /*This program uses loops
+         * to return odd numbers
+         * between 1 and 100 
+         * nicole brown 10/20/2016*/
+
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+
+
+            
+                {
+                    Console.WriteLine(" " + i);
+                }
+
+
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+
+
+        }//end main
+    }//end class
+}//end namespace
