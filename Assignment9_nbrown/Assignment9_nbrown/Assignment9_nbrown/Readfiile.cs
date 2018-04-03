0b0c672a128fc1dc242ae6bcd94153115915df92
nbrownseattle <nbrown28@seattlecentral.edu>
2018-04-03 12:36:13 -0700

    Clean up files for assignment 9

---
+++ Assignment9_nbrown/Assignment9_nbrown/Assignment9_nbrown/Readfiile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //added this
+
+namespace Assignment9_nbrown
+{
+   public class ReadFile
+    {
+       private StreamReader reader;
+       private string filepath = null;
+
+        public ReadFile(string path)
+        {
+            filepath = path;
+
+        }
+
+        public string GetFile()
+        {
+            string line = null;
+            try
+            {
+                reader = new StreamReader(filepath);
+                line = reader.ReadToEnd();
+            }
+            catch(FileNotFoundException fnfex)
+            {
+                throw fnfex;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            return line;
+
+        }
+    }
+}
---
+++ Assignment9_nbrown/Assignment9_nbrown/Assignment9_nbrown/RightFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //added this Input Object
+
+namespace Assignment9_nbrown
+{
+    class RightFile //WriteFile
+    {
+        private StreamWriter writer;
+        string filepath = null;
+
+        public RightFile(string path)
+        {
+            try
+            {
+                filepath = path;
+                writer = new StreamWriter(path, true);
+            }
+            
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void AddText(string line)
+        {
+            writer.Write(line); //writes whatever we pass into the file
+        }
+
+        public void CloseFile()
+        {
+            writer.Close();
+        }
+    }
+}
